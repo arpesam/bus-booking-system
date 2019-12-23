fc71de495909da91d8d2fc72626710557229d286
Danilo <daniloarpesam@gmail.com>
2019-12-23 17:11:59 -0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    openWin() {
+      const routeData = this.$router.resolve({name: 'print' });
+      console.log(routeData.href)
+      window.open(routeData.href, "myWindow", "width=1018px, height=980px");
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Print.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hello-world />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
